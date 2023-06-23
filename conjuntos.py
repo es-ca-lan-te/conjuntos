ca66f73a673dc840f197a3600dd713a3a83e8bba
es-ca-lan-te <134089928+es-ca-lan-te@users.noreply.github.com>
2023-06-23 15:57:06 -0300

    Add files via upload

---
+++ conjuntos.py
@@ -0,0 +1,23 @@
+"""
+Programa Conjuntos
+Descrição: ___
+Autor: Ricardo.
+Data: 23/06/2023
+Versão: 0.0.1
+"""
+
+
+# Atribuição de Variáveis
+conjunto_1 = set([99, 8, 1, 1, 2, 3, 5, 8, 151561])
+
+
+# Entrada de dados
+
+
+# Processamento de dados
+conjunto_1.add(10)
+conjunto_1.remove(1)
+
+
+# Saída de dados
+print(conjunto_1)
